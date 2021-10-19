74ca9fd5b31248694eacc927cc09ed8575f27b50
Mark Tripney <marktripney@icloud.com>
2021-10-19 21:45:39 +0200

    Add grid; add General & Bio to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,34 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { Bio } from './Bio';
+import { General } from './General';
 
-export const Main = (props) => {
-  return <h1>{props.userInfo.name}</h1>;
+const MainContainer = styled.main`
+  background: ${({ theme }) => theme.sectionBackground};
+  border-radius: 15px;
+  padding: 1.5rem;
+  box-shadow: ${({ theme }) => theme.shadow};
+  display: grid;
+  column-gap: 1em;
+  grid-template-columns: 70px 1fr;
+  grid-template-areas:
+    'general general'
+    'bio bio'
+    'stats stats';
+  @media (min-width: 600px) {
+    grid-template-columns: 117px 1fr;
+    grid-template-areas:
+      'general general'
+      '. bio'
+      '. stats';
+  }
+`;
+
+export const Main = ({ userInfo }) => {
+  return (
+    <MainContainer>
+      <General userInfo={userInfo} />
+      <Bio bio={userInfo.bio} />
+    </MainContainer>
+  );
 };
