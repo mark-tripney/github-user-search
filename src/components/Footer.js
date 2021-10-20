b598b74f4c2bdf1d1de2da7e9a4460220adcc291
Mark Tripney <marktripney@icloud.com>
2021-10-20 13:33:45 +0200

    Change order of elements on wider screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { FooterInfo } from './FooterInfo';
 import { ReactComponent as MapPin } from '../images/icon-location.svg';
 import { ReactComponent as Website } from '../images/icon-website.svg';
 import { ReactComponent as Twitter } from '../images/icon-twitter.svg';
@@ -13,6 +14,14 @@ const FooterContainer = styled.footer`
   margin-bottom: 3rem;
   @media (min-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
+    div:nth-child(2) {
+      grid-column: 1/2;
+      grid-row: 2/3;
+    }
+    div:nth-child(3) {
+      grid-column: 2/3;
+      grid-row: 1/2;
+    }
   }
   @media (min-width: 900px) {
     gap: 1em 2em;
@@ -24,20 +33,9 @@ const FooterInfoContainer = styled.div`
   align-items: center;
   column-gap: 0.8125em;
   row-gap: 1em;
+  opacity: ${(props) => (!props.opaque ? '0.5' : '1')};
   svg path {
     fill: ${({ theme }) => theme.footerImgColor};
-  }
-  svg {
-    opacity: ${(props) => (!props.opaque ? '0.5' : '1')};
-  }
-`;
-
-const FooterInfoText = styled.p`
-  opacity: ${(props) => (!props.opaque ? '0.5' : '1')};
-  font-size: 0.8125em;
-  margin: 0;
-  @media (min-width: 768px) {
-    font-size: 0.9375em;
   }
 `;
 
@@ -45,33 +43,20 @@ export const Footer = (props) => {
   return (
     <FooterContainer>
       <FooterInfoContainer opaque={props.userInfo.location}>
-        <MapPin alt="Map-pin icon" opaque={props.userInfo.location} />
-        <FooterInfoText opaque={props.userInfo.location}>
-          {props.userInfo.location ? props.userInfo.location : 'Not available'}
-        </FooterInfoText>
+        <MapPin alt="Map-pin icon" />
+        <FooterInfo data={props.userInfo.location} />
       </FooterInfoContainer>
-
       <FooterInfoContainer opaque={props.userInfo.blog}>
-        <Website alt="Link icon" />
-        <FooterInfoText opaque={props.userInfo.blog}>
-          {props.userInfo.blog ? props.userInfo.blog : 'Not available'}
-        </FooterInfoText>
+        <Website alt="Wesbite icon (a linked chain)" />
+        <FooterInfo opaque={props.userInfo.blog} data={props.userInfo.blog} />
       </FooterInfoContainer>
-
       <FooterInfoContainer opaque={props.userInfo.twitter_username}>
-        <Twitter alt="Twitter icon" />
-        <FooterInfoText opaque={props.userInfo.twitter_username}>
-          {props.userInfo.twitter_username
-            ? props.userInfo.twitter_username
-            : 'Not available'}
-        </FooterInfoText>
+        <Twitter alt="Twitter logo" />
+        <FooterInfo data={props.userInfo.twitter_username} />
       </FooterInfoContainer>
-
       <FooterInfoContainer opaque={props.userInfo.company}>
-        <Company alt="Company icon" />
-        <FooterInfoText opaque={props.userInfo.company}>
-          {props.userInfo.company ? props.userInfo.company : 'Not available'}
-        </FooterInfoText>
+        <Company alt="Company icon (an office block)" />
+        <FooterInfo data={props.userInfo.company} />
       </FooterInfoContainer>
     </FooterContainer>
   );
